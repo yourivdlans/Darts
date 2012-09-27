79bf5f09b24ae3bb54a2eb75154b304b89fec263
Joshua Jansen <joshua@itflows.nl>
2012-09-27 16:58:04 +0200

    Made RootviewController.xib default view to load, added  sliders & fire button and dartboard

--- Darts2/RootViewController.m
+++ Darts2/RootViewController.m
@@ -14,7 +14,7 @@
 
 @implementation RootViewController
 
-@synthesize settingsViewController;
+@synthesize settingsViewController, verticalSlider, horizontalSlider, verticalPosition, horizontalPosition;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,6 +28,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    CGAffineTransform trans = CGAffineTransformMakeRotation(M_PI * 0.5);
+    verticalSlider.transform = trans;
+    
+    UIGraphicsBeginImageContext(self.view.frame.size);
+    [[UIImage imageNamed:@"0930Dartbord.jpeg"] drawInRect:self.view.bounds];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:image];
+
 	// Do any additional setup after loading the view.
 }
 
@@ -35,6 +45,36 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)updatePositions:(id)sender
+{
+    UISlider * slider = (UISlider*) sender;
+    int stand = (int)(slider.value+0.5F);
+    
+    if( slider.tag == 0 ){
+        NSString * labelValue = [[NSString alloc] initWithFormat:@"Horizontal: %d", stand];
+        horizontalPosition.text = labelValue;
+        [labelValue release];
+    }else if( slider.tag == 1 ){
+        NSString * labelValue = [[NSString alloc] initWithFormat:@"Vertical: %d", stand];
+        verticalPosition.text = labelValue;
+        [labelValue release];
+    }    
+}
+
+- (IBAction)fire
+{
+    int horizontal = (int)(horizontalSlider.value+0.5F);
+    int vertical = (int)(verticalSlider.value+0.5F);
+    
+    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"Je hebt gegooid!"
+                                                      message: [[NSString alloc] initWithFormat:@"horizontal: %d vertical: %d", horizontal, vertical]
+                                                     delegate:nil
+                                            cancelButtonTitle:@"OK"
+                                            otherButtonTitles:nil];
+    [message show];
+    [message release];
 }
 
 - (IBAction)showSettings
